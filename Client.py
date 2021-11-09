668f7b4bf8a934b956e423cb8d5e68901e6a0b8a
arbaretaz.aurelien <aurelien.arbaretaz@orange.fr>
2021-11-09 18:01:13 +0100

    maj client et select upload sur https://mybucket199973.s3.amazonaws.com/select.html

--- Client.py
+++ Client.py
@@ -6,22 +6,20 @@ sqs = boto3.resource('sqs')
 requestQueue = sqs.get_queue_by_name(QueueName='requestQueue')
 responseQueue = sqs.get_queue_by_name(QueueName='responseQueue')
 message=None
+# Demande les entrée que nous allons envoyer à la queue de requête
 entre=input('Entrer jusque 10 nombre espacé de 1:')
 response = requestQueue.send_message(MessageBody=entre)
+# Boucle qui attend la réponse
 while(message is None):
+    # Message d'attente
     print("wait")  
-    for message in responseQueue.receive_messages(MessageAttributeNames=['Author']):
-    # Get the custom author message attribute if it was set
-        author_text = ''
-        if message.message_attributes is not None:
-            author_name = message.message_attributes.get('Author').get('StringValue')
-            if author_name:
-                    author_text = '({0})'.format(author_name)
-        # Print out the body and author (if set)
-        print('{0}'.format(message.body, author_text))
-    
-        # Let the queue know that the message is processed
+    # Prends le message dans la queue de réponse
+    for message in responseQueue.receive_messages():
+        # Affiche le body du message de la responseQueue
+        print('{0}'.format(message.body))  
+        # Dire à la queue qu'elle peut supprimer le message
         message.delete()
+    # Tempo avant de request encore
     time.sleep(5)
 
     
